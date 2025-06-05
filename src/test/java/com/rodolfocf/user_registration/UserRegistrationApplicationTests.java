34d9a2a705f4e0b98db36aa63355d1a497627141
Rodolfo Cunhasque Faedo <faedorodolfo.programming@gmail.com>
2025-06-05 15:21:19 -0300

    first commit

---
+++ src/test/java/com/rodolfocf/user_registration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodolfocf.user_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
